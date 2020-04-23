d015e306be8ca9ab8447f9fa6d3f4ccae6b800c9
Amy Stefani <amystefani1@gmail.com>
2020-04-22 18:18:45 -0700

    updating for heroku deployment

--- src/common/ApiConstants.js
+++ src/common/ApiConstants.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:8080"
-// export const API_URL = "https://potluck-planner-server-43138.herokuapp.com"
+// export const API_URL = "http://localhost:8080"
+export const API_URL = "https://potluck-planner-server-43138.herokuapp.com"
 
 export const X_RAPIDAPI_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/";
 export const X_RAPIDAPI_HOST = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
